01d55448756987d7a30768cda2e1bdf5b2a0adb5
Halil Akgun <halilakgun0@gmail.com>
2024-10-03 20:39:51 +0200

    day2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!!!");
+const express = require("express");
+
+const { connectToDb, getDb } = require("./db");
+
+const app = express();
+
+let db;
+
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3001, () => {
+            console.log("Listening on port 3001");
+        });
+
+        db = getDb();
+    }
+})
---
+++ db.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+
+let dbConnection;
+
+let uri = 'mongodb://0.0.0.0:27017/dbnxtwave'; // 0.0.0.0 is localhost
+
+module.exports = {
+    connectToDb: (callback) => {
+        MongoClient.connect(uri)
+            .then(client => {
+                dbConnection = client.db();
+                return callback();
+            })
+            .catch(err => {
+                console.log(err);
+                return callback(err);
+            })
+    },
+
+    getDb: () => dbConnection
+}
+
